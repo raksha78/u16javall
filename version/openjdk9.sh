0f1e07109a7c43bc9a704773492ee33eb0366780
raksha78 <rakshitha27@gmail.com>
2017-07-17 13:42:33 +0530

    update java

--- version/openjdk9.sh
+++ version/openjdk9.sh
@@ -9,4 +9,4 @@ echo "================ Installing openjdk-9-jdk ================="
 sudo apt install openjdk-9-jdk
 sudo dpkg --configure -a
 sudo dpkg -i --force-overwrite '/var/cache/apt/archives/openjdk-9-jdk_9~b114-0ubuntu1_amd64.deb'
-
+sudo apt-get remove openjdk-9-jdk-headless
