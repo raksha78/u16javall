8e4160aa6d8106ccde083c10deccbb67c7bd5665
raksha78 <rakshitha27@gmail.com>
2017-07-17 18:12:53 +0530

    update java

--- version/oraclejdk9.sh
+++ version/oraclejdk9.sh
@@ -4,8 +4,9 @@ echo "================ Installing oracle-java9-installer ================="
 #echo oracle-java9-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+sudo update-alternatives --config java 
 sudo apt-get install -y oracle-java9-installer
-sudo update-alternatives --config java
+
 
 echo 'export JAVA_HOME=/usr/lib/jvm/java-9-oracle' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/lib/jvm/java-9-oracle/jre/bin' >> $HOME/.bashrc
