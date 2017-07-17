7f8df6ee5d8c3527aa5eab4f152d69b598067531
raksha78 <rakshitha27@gmail.com>
2017-07-17 16:06:21 +0530

    update java

--- version/openjdk9.sh
+++ version/openjdk9.sh
@@ -3,11 +3,10 @@
 echo "================ Installing openjdk-9-jdk ================="
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get -o Dpkg::Options::="--force-overwrite" install openjdk-9-jdk
 sudo apt-get remove openjdk-9-jdk-headless
-#sudo apt-get install -y openjdk-9-jdk
-#sudo apt-get remove openjdk-9-jdk-headless
-#sudo apt-get -o Dpkg::Options::="--force-overwrite" install openjdk-9-jdk
+sudo apt install openjdk-9-jdk
+
+
 
 sudo update-alternatives --set java /usr/lib/jvm/java-9-openjdk-amd64/jre/bin/java
 sudo update-alternatives --set javac /usr/lib/jvm/java-9-openjdk-amd64/bin/javac
@@ -17,3 +16,6 @@ sudo apt-get install icedtea-9-plugin
 sudo update-alternatives --set javaws /usr/lib/jvm/java-9-openjdk-amd64/jre/bin/javaws
 echo 'export JAVA_HOME=/usr/lib/jvm/java-9-openjdk-amd64' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/lib/jvm/java-9-openjdk-amd64/jre/bin' >> $HOME/.bashrc
+
+
+
