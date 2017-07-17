d65fdfe945387e3fa4e74f2e15098a7fb934d0bd
raksha78 <rakshitha27@gmail.com>
2017-07-17 16:33:51 +0530

    update java

--- version/openjdk9.sh
+++ version/openjdk9.sh
@@ -3,7 +3,8 @@
 echo "================ Installing openjdk-9-jdk ================="
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt install openjdk-9-jdk
+sudo apt-get install openjdk-9-jdk
+sudo apt-get remove openjdk-9-jdk-headless
 
 
 
